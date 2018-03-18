6220b5dac2152085ae21cf98065d1ad6eb2ebb1a
Cody Gates <cody.gates.89@gmail.com>
2018-03-18 00:11:25 -0600

    add contenteditable feature to list items

--- toDo.js
+++ toDo.js
@@ -6,7 +6,7 @@ $("#inputBox").keyup(function(event){
     if(event.which === 13) {
         let addToDo = $(this).val();
         $(this).val("");
-        $("ul").append("<li><span>" + addToDo + "</span></li>");
+        $("ul").append("<li contenteditable=\"true\"><span>" + addToDo + "</span></li>");
     }
 });
 
