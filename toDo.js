19bf49de0438b032109634a608e2acac9921ed62
Cody Gates <cody.gates.89@gmail.com>
2018-03-20 22:09:28 -0600

    change a few button layouts and functionality

--- toDo.js
+++ toDo.js
@@ -1,12 +1,54 @@
-$("ul").on("click", "li", function(){
-    $(this).css("color", "gray");
-});
+// $("ul").on("click", "li", function(){
+//     $(this).css("color", "gray");
+// });
+
+function appendItems() {
+    $("ul").append("<li class=\"item\" contenteditable=\"true\"><p>" + addToDo + "</p></li>");
+    // $("ul").append("<li class=\"item\" contenteditable=\"true\"><p>" + addToDo + "</p></li>");
+    // $("ul").append("<li class=\"item\" contenteditable=\"true\"><p>" + addToDo + "</p></li>");
+    // $("ul").append("<li class=\"item\" contenteditable=\"true\"><p>" + addToDo + "</p></li>");
+}
+
+
 
 $("#inputBox").keyup(function(event){
     if(event.which === 13) {
         let addToDo = $(this).val();
         $(this).val("");
-        $("ul").append("<li contenteditable=\"true\"><span>" + addToDo + "</span></li>");
+        appendItems();
     }
 });
 
+
+
+
+
+///
+
+
+function markComplete(myElement) {
+    let myDad = $(myElement).parent().parent();
+    $(myDad).toggleClass("complete");
+    $(myDad).find(".iconSpot").toggleClass("fa fa-check-square");
+}
+
+function removeRow(myElement) {
+    let grandDad = $(myElement).parent().parent();
+    $(grandDad).slideUp("fast", function(){
+        $(grandDad).remove();
+    });
+}
+
+////another option
+
+function removeRow(theElement){
+    let granDad = $(theElement).parent().parent();
+
+    $(granDad).animate({
+        opacity: 0,
+        left: "+=50"
+    }, 800, function() {
+        // Animation complete.
+        $(granDad).remove();
+    });
+}
